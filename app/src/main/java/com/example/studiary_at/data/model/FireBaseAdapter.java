e4dcc7db8c485cb83eab0159659de80d35cd94cc
llucsv7 <llucslatosch@gmail.com>
2021-04-23 12:03:17 +0200

    update firebase

--- app/src/main/java/com/example/studiary_at/data/model/FireBaseAdapter.java
+++ app/src/main/java/com/example/studiary_at/data/model/FireBaseAdapter.java
@@ -1,5 +1,11 @@
 package com.example.studiary_at.data.model;
 
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import java.io.FileInputStream;
+
 public class FireBaseAdapter {
     //Connect to firebase when we have a calendar:
     // Write a message to the database
@@ -25,53 +31,14 @@ public class FireBaseAdapter {
         }
     });*/
 
-    /* File JSON:
     FileInputStream serviceAccount =
-            new FileInputStream("C:\Program Files\Android\studiaryat-firebase-adminsdk-fcyyz-53242785dd.json");
+            new FileInputStream("C:\Program Files\Android\studiary-at-firebase-adminsdk-ek8dp-d947414867.json");
 
     FirebaseOptions options = new FirebaseOptions.Builder()
             .setCredentials(GoogleCredentials.fromStream(serviceAccount))
             .setDatabaseUrl("https://studiaryat-default-rtdb.europe-west1.firebasedatabase.app")
             .build();
 
-    FirebaseApp.initializeApp(options);*/
+    FirebaseApp.initializeApp(options);
 
-    /* FIREBASE-->Sign in
-    //In your sign-in activity's onCreate method, get the shared instance of the FirebaseAuth object:
-    private FirebaseAuth mAuth;
-    // ...
-    // Initialize Firebase Auth
-    mAuth = FirebaseAuth.getInstance();
-
-    //Then, when initializing your Activity, check to see if the user is currently signed in:
-    @Override
-    public void onStart() {
-    super.onStart();
-    // Check if user is signed in (non-null) and update UI accordingly.
-    FirebaseUser currentUser = mAuth.getCurrentUser();
-    updateUI(currentUser);
-
-    //After you receive the custom token from your authentication server, pass it to signInWithCustomToken to sign in the user:
-     mAuth.signInWithCustomToken(mCustomToken)
-        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithCustomToken:success");
-                    FirebaseUser user = mAuth.getCurrentUser();
-                    updateUI(user);
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithCustomToken:failure", task.getException());
-                    Toast.makeText(CustomAuthActivity.this, "Authentication failed.",
-                            Toast.LENGTH_SHORT).show();
-                    updateUI(null);
-                }
-            }
-        });
-
-        //Sing out a user
-        FirebaseAuth.getInstance().signOut();
-     */
 }
