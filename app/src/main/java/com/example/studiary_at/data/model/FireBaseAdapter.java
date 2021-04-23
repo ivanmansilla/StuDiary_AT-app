2fdc2efc2c4588538ecfcd8ec869d8af975428b2
ivanmansilla <ivanmansillaflores@gmail.com>
2021-04-23 12:19:45 +0200

    FireBase Sing in and Sign up

--- app/src/main/java/com/example/studiary_at/data/model/FireBaseAdapter.java
+++ app/src/main/java/com/example/studiary_at/data/model/FireBaseAdapter.java
@@ -1,35 +1,11 @@
 package com.example.studiary_at.data.model;
 
-
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
 import java.io.FileInputStream;
 
 public class FireBaseAdapter {
-    //Connect to firebase when we have a calendar:
-    // Write a message to the database
-    /*FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("message");
-
-    myRef.setValue("Hello, World!");*/
-
-    /* Read from the database
-    myRef.addValueEventListener(new ValueEventListener() {
-        @Override
-        public void onDataChange(DataSnapshot dataSnapshot) {
-            // This method is called once with the initial value and again
-            // whenever data at this location is updated.
-            String value = dataSnapshot.getValue(String.class);
-            Log.d(TAG, "Value is: " + value);
-        }
-
-        @Override
-        public void onCancelled(DatabaseError error) {
-            // Failed to read value
-            Log.w(TAG, "Failed to read value.", error.toException());
-        }
-    });*/
 
     FileInputStream serviceAccount =
             new FileInputStream("C:\Program Files\Android\studiary-at-firebase-adminsdk-ek8dp-d947414867.json");
@@ -41,4 +17,41 @@ public class FireBaseAdapter {
 
     FirebaseApp.initializeApp(options);
 
+/*
+    //Sign in in other lenguage
+    function signInWithEmailPassword() {
+        var email = "test@example.com";
+        var password = "hunter2";
+        // [START auth_signin_password]
+        firebase.auth().signInWithEmailAndPassword(email, password)
+                .then((userCredential) => {
+                // Signed in
+                var user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+                var errorCode = error.code;
+        var errorMessage = error.message;
+    });
+        // [END auth_signin_password]
+    }
+------------------------------------------------------------------------------------
+    //Sign up in other lenguage
+    function signUpWithEmailPassword() {
+        var email = "test@example.com";
+        var password = "hunter2";
+        // [START auth_signup_password]
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+                .then((userCredential) => {
+                // Signed in
+                var user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+                var errorCode = error.code;
+        var errorMessage = error.message;
+        // ..
+    });
+        // [END auth_signup_password]
+    }*/
 }
