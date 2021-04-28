7dfdb9637eba2dd6d1a0a18ce8dd8f9c2ffdfb44
julio <julio.w2001@gmail.com>
2021-04-28 03:30:07 +0200

    calendar fragment (?

--- app/src/main/java/com/example/studiary_at/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/studiary_at/ui/calendar/CalendarFragment.java
@@ -1,10 +1,18 @@
 package com.example.studiary_at.ui.calendar;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class CalendarFragment extends Fragment {
 
     private CalendarViewModel calendarViewModel;
+
 /*
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -21,4 +29,44 @@ public class CalendarFragment extends Fragment {
         return root;
     }
          #FALTA AÑADIR EL FRAGMENT_CALENDAR AL BOTTOM NAVIGATION MENU#    */
+
+    /*
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDisplayDate = (TextView) findViewById(R.id.tvDate);
+
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        MainActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
+
+                String date = month + "/" + day + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
+    }
+}
+
+ */
 }
--- app/src/main/java/com/example/studiary_at/ui/calendar/CalendarViewModel.java
+++ app/src/main/java/com/example/studiary_at/ui/calendar/CalendarViewModel.java
@@ -1,5 +1,7 @@
 package com.example.studiary_at.ui.calendar;
 
+import android.widget.CalendarView;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -12,6 +14,9 @@ public class CalendarViewModel extends ViewModel {
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment");
     }
+
+    private CalendarView mCalendarView;
+
 
     public LiveData<String> getText() {
         return mText;
