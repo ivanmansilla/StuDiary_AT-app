20b614c2a148deb6cdc7073f0d3f7bd0bfa535d8
ivanmansilla <ivanmansillaflores@gmail.com>
2021-04-28 10:09:07 +0200

    Calendar Bottom Navigation

--- app/src/main/java/com/example/studiary_at/MainActivity.java
+++ app/src/main/java/com/example/studiary_at/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.studiary_at;
 
 import android.os.Bundle;
 
-import com.example.studiary_at.ui.login.ForgotPasswordFragment;
-import com.example.studiary_at.ui.login.LoginFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,7 +21,7 @@ public class MainActivity extends AppCompatActivity  {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_login, R.id.navigation_notes, R.id.navigation_settings)
+                R.id.navigation_login, R.id.navigation_calendar, R.id.navigation_settings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
--- app/src/main/java/com/example/studiary_at/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/studiary_at/ui/calendar/CalendarFragment.java
@@ -4,31 +4,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import com.example.studiary_at.R;
 
 public class CalendarFragment extends Fragment {
 
     private CalendarViewModel calendarViewModel;
 
-/*
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         calendarViewModel =
                 new ViewModelProvider(this).get(CalendarViewModel.class);
         View root = inflater.inflate(R.layout.fragment_calendar, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
+        final CalendarView textView = root.findViewById(R.id.calendarView);
         calendarViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                //textView.setText(s);
             }
         });
         return root;
     }
-         #FALTA AÑADIR EL FRAGMENT_CALENDAR AL BOTTOM NAVIGATION MENU#    */
+        /*#FALTA AÑADIR EL FRAGMENT_CALENDAR AL BOTTOM NAVIGATION MENU#    */
 
     /*
     @Override
