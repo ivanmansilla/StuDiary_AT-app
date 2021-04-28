0a0143686d38654686f5e6bdf7f6d2f0fa04f514
llucsv7 <llucslatosch@gmail.com>
2021-04-28 09:56:34 +0200

    gradle 2

--- app/src/main/java/com/example/studiary_at/data/model/FireBaseAdapter.java
+++ app/src/main/java/com/example/studiary_at/data/model/FireBaseAdapter.java
@@ -1,20 +1,27 @@
 package com.example.studiary_at.data.model;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class FireBaseAdapter {
 
-    /*FileInputStream serviceAccount =
-            new FileInputStream("C:\Program Files\Android\studiary-at-firebase-adminsdk-ek8dp-d947414867.json");
 
-    FirebaseOptions options = new FirebaseOptions.Builder()
-            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-            .setDatabaseUrl("https://studiaryat-default-rtdb.europe-west1.firebasedatabase.app")
-            .build();
 
-    FirebaseApp.initializeApp(options);*/
+    public FireBaseAdapter() throws IOException {
+        FileInputStream serviceAccount =
+                new FileInputStream("C:\Program Files\Android\studiary-at-firebase-adminsdk-ek8dp-d947414867.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://studiaryat-default-rtdb.europe-west1.firebasedatabase.app")
+                .build();
+        FirebaseApp.initializeApp(options);
+    }
+
+
 
 }
